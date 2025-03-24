15631bbe45f3215ce44dc707779d9c08d1d2342c
Jan Leirvaag <lja022025@gmail.com>
2025-03-24 14:51:48 +0100

    update

--- obj/Debug/net9.0/Petshop.AssemblyInfo.cs
+++ obj/Debug/net9.0/Petshop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1bd96aaad19ad19d4d135b36342986fd5dfbbac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e871e6a66c55749f14bc26bf6544919e3da2d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
